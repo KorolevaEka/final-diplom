7c13064a1d2ba591f19b9ea7f854f4137e09ff3d
KorolevaEka <111436891+KorolevaEka@users.noreply.github.com>
2024-01-07 21:32:35 +0100

    Update TestData.java

--- src/test/java/github/data/TestData.java
+++ src/test/java/github/data/TestData.java
@@ -6,7 +6,6 @@ import lombok.Data;
 @Data
 public class TestData {
     private final String
-            baseUrl = System.getProperty("baseUrl","https://github.com/"),
             ownerName = System.getProperty("ownerName", "KorolevaEka"),
             repoUnderTest = System.getProperty("repoUnderTest", "KorolevaEka/HW9_faker"),
             searchRepo = System.getProperty("searchRepo", "KorolevaEka/HW9_faker"),
@@ -14,8 +13,8 @@ public class TestData {
             advancedSearchLanguage = System.getProperty("advancedSearchLanguage", "Java"),
             advancedSearchFrom = System.getProperty("advancedSearchFrom", "KorolevaEka"),
             advancedSearchDate = System.getProperty("advancedSearchDate", "2023"),
-            urlSearchAdvanced = baseUrl + "search/advanced",
-            urlLoginPage = baseUrl + "login",
+            urlSearchAdvanced = "https://github.com/search/advanced",
+            urlLoginPage = "https://github.com/login",
             targetFileName = "README.md",
             negativeTargetFileName = "invalid_file.txt",
             pageTitle = "GitHub: Let’s build from here · GitHub";
